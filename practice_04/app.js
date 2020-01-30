897d20a5c7dcb206a506fd8178960975ac6e3dc5
WaniFi <chaos0824@naver.com>
2020-01-30 11:45:34 +0900

    입력한 이메일에 대해 반응하는 서버 구현

--- practice_04/app.js
+++ practice_04/app.js
@@ -14,7 +14,7 @@ var connection = mysql.createConnection({
     port : 3306,                // 연동할 서버 Port 번호
     user : 'root',              // 연동할 DB 주인 계정
     password : 'gktprkdhk24',   // 연동할 DB 주인 비밀번호
-    database : 'jaman'          // 연동할 DB 명
+    database : 'jsman'          // 연동할 DB 명
 });
 // 정의된 connection 객체로 연결 시도
 connection.connect();
@@ -65,11 +65,33 @@ app.post('/email_post', function(req, res){
 
 // form.html의 ajax send 버튼에 대한 처리
 app.post('/ajax_send_email', function(req, res){
-    // 전송 받은 데이터를 JSON 데이터로 새로 정의
-        // 해당 코드는 이후에 DB를 통해 DB 내에 데이터가 존재하는지 확인하는 과정으로 수정할 예정임
-    var response_data = {'result' : 'ok', 'email' : req.body.email}
-    // 새로 정의한 JSON 데이터를 javascript(main.js)로 전송
-    res.json(response_data)
+    // POST 형식으로 온 email 변수 정의
+    var email = req.body.email;
+    // DB에 전송할 json 변수 정의
+    var response_data = {};
+
+    // email 정보에 대한 DB 제어 코드 정의
+    var query = connection.query('select name from user where email="'+email+'"', 
+                // DB query에 대한 결과 입력
+                function(err, rows){
+                    // 에러일 경우, err를 날림
+                    if(err) throw err;
+                    // 변수 rows가 null이 아닐 경우,
+                    if(rows[0]){
+                        // 데이터가 존재함을 정의 및 해당 데이터를 입력
+                        response_data.result = "ok";
+                        response_data.name = rows[0].name;
+                    } else{
+                        // 데이터가 존재하지 않음을 정의
+                        response_data.result = "none";
+                        response_data.name = "";
+                    }
+
+                    // 새로 정의한 JSON 데이터를 javascript(main.js)로 전송
+                    res.json(response_data)
+                });
+
+    // res.json이 비동기로 실행되기 위해 위치 변경
 })
 
 console.log("End of Server code . . .");
